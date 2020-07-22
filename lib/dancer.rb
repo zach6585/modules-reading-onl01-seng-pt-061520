b8105d74b9c7af53f8241ca7c1f982356885920c
zachary mines <zmines@wesleyan.edu>
2020-07-22 00:37:49 +0000

    Done.

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,2 +1,9 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
+require_relative './fancy_dance.rb'
+
+class Dancer 
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
+  include Dance
+end 
